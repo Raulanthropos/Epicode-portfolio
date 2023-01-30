73674f173453dec7668cda5915776b4e434847a1
Raulanthropos <ipsichias@yandex.com>
2023-01-30 08:52:10 +0200

    form works - email sent directly. Todo: Hero section - modify form content that's sent - notification window for better UX

---
+++ environment.js
@@ -0,0 +1,5 @@
+const environment = {
+    MY_SERVICE_ID: "service_x4owk7q",
+    MY_TEMPLATE: "template_ceejems",
+    MY_PUBLIC_KEY: "GoygN0xGfGhvIxjY0"
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cards = document.querySelectorAll(".card");
 const cardContainer = document.getElementById("cards");
+const myForm = document.getElementById("my-form");
 
 const handleOnMouseMove = (e) => {
   const { currentTarget: target } = e;
@@ -7,22 +8,29 @@ const handleOnMouseMove = (e) => {
   const rect = target.getBoundingClientRect(),
     x = e.clientX - rect.left,
     y = e.clientY - rect.top;
-
   target.style.setProperty("--mouse-x", `${x}px`);
   target.style.setProperty("--mouse-y", `${y}px`);
 };
 
-for (const card of cards) {
-  card.onmousemove = (e) => handleOnMouseMove(e);
-}
+emailjs.init(environment.MY_SERVICE_ID);
 
-// cardContainer.onmousemove = (e) => {
-//   for (const card of cards) {
-//     const rect = target.getBoundingClientRect(),
-//       x = e.clientX - rect.left,
-//       y = e.clientY - rect.top;
+myForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  fetch("api.emailjs.com/api/v1.0/email/send", {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${environment.MY_SERVICE_ID}`
+    },
+  })
+  emailjs.sendForm(environment.MY_SERVICE_ID, environment.MY_TEMPLATE, myForm, environment.MY_PUBLIC_KEY)
+    .then(function(response) {
+      if (response.ok) {
+        console.log("Email successfully sent!", response.status);
+      }
+    }, function(error) {
+      console.error("Failed to send email:", error);
+    });
+});
 
-//     card.style.setProperty("--mouse-x", `${x}px`);
-//     card.style.setProperty("--mouse-y", `${y}px`);
-//   }
-// };
+
+
