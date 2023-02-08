cbfecb0737d3e2fa9ac7bb52217a63ac2cbca9cb
Raulanthropos <ipsichias@yandex.com>
2023-02-08 21:13:56 +0200

    first link works as intended, need to implement the rest with accurate snapshots and reliable links

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const cards = document.querySelectorAll(".card");
 const cardContainer = document.getElementById("cards");
 const myForm = document.getElementById("my-form");
+const cardContents = document.querySelector('.card');
+console.log(cardContents);
 
-const handleOnMouseMove = (e) => {
-  const { currentTarget: target } = e;
-
-  const rect = target.getBoundingClientRect(),
-    x = e.clientX - rect.left,
-    y = e.clientY - rect.top;
-  target.style.setProperty("--mouse-x", `${x}px`);
-  target.style.setProperty("--mouse-y", `${y}px`);
-};
+cardContents.addEventListener("click", function() {
+  window.open("https://linked-in-clone-proj.vercel.app/", "_blank");
+});
 
 emailjs.init(environment.MY_SERVICE_ID);
 
